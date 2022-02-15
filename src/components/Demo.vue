7872bbd8a020b639d3322e20d60b2ee41bcc16d6
nambt <buithanhnam0302@gmail.com>
2022-02-15 14:23:55 +0700

    feat:add Demo component, example using data,computed,method

---
+++ src/components/Demo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="demo">
+    <h1>{{ message }}</h1>
+    <h1>{{ upperMessage }}</h1>
+    <h1>{{ addPrefixMessage() }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data: function() {
+    return {
+      message: 'Demo VueJS'
+    };
+  },
+
+  computed: {
+    upperMessage() {
+      return this.message.toUpperCase();
+    }
+  },
+
+  methods: {
+    addPrefixMessage() {
+      return 'Title: ' + this.message;
+    }
+  }
+};
+</script>
