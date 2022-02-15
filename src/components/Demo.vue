d148cae5c8eefc46be21029459b6cfde9e6a0e06
nambt <buithanhnam0302@gmail.com>
2022-02-15 16:10:14 +0700

    feat: example vuejs lifecycle

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -3,6 +3,7 @@
     <h1>{{ message }}</h1>
     <h1>{{ upperMessage }}</h1>
     <h1>{{ addPrefixMessage() }}</h1>
+    <button @click="updateMessage(' Updated!')">Update message</button>
   </div>
 </template>
 
@@ -24,7 +25,46 @@ export default {
   methods: {
     addPrefixMessage() {
       return 'Title: ' + this.message;
+    },
+
+    updateMessage(param) {
+      this.message +=  param;
     }
+  },
+
+  beforeCreate() {
+    console.log('Before create');
+    // this.message is underfined
+    console.log('Message: ', this.message);
+  },
+
+  created() {
+    console.log('Created');
+    // this.message is underfined
+    console.log('Message: ', this.message);
+  },
+
+  beforeMount() {
+    console.log('Before mount');
+    // this.el is underfined
+    console.log(this.$el);
+  },
+
+  mounted () {
+    console.log('Mounted')
+    console.log(this.$el.id)
+  },
+
+  beforeUpdate () {
+    console.log('Before update');
+    // this.message is underfined
+    console.log('Message: ', this.message);
+  },
+
+  updated() {
+    console.log('Update');
+    // this.message is underfined
+    console.log('Message: ', this.message);
   }
 };
 </script>
