8175465345802b0b3e072059671a993d06751cf0
nambt <buithanhnam0302@gmail.com>
2022-02-16 16:20:06 +0700

    feat: example for communications beetween component, prop, emit

---
+++ src/components/Post.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="post">
+    <div class="post-content">
+      <h2>{{ post.title }}</h2>
+      <p :style="{ fontSize: size + 'px' }">{{ post.description }}</p>
+    </div>
+    <div class="post-action">
+      <button class="change-size" @click="$emit('change-size', sizeChange)">+</button>
+      <button class="change-size" @click="$emit('change-size', -sizeChange)">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      sizeChange: 1,
+    }
+  },
+  name: 'Post',
+  props: ['post', 'size'],
+};
+</script>
+
+<style scoped>
+.post {
+  margin-bottom: 50px;
+}
+
+button.change-size {
+  width: 25px;
+  cursor: pointer;
+}
+</style>
